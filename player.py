86922a919179d19ad9a53ef967c40f1bb16eea34
jsm <joshuamaza209@gmail.com>
2018-06-21 18:23:14 -0700

    updated player class, added a todo list to designdoc

--- player.py
+++ player.py
@@ -1,16 +1,16 @@
 class Player:
 
-
-
     def __init__(self):
         self.resources = [0, 0, 0, 0, 100, 0, 0]
         self.resource_names = ["Credits", "Food", "Fuel", "Hull", "Stress Level", "Crew", "Wisdom"]
         # Resources: Credits, Food, Fuel, Hull, Stress, Crew, Wisdom
 
-
     def get_count(self):
-        for x in self.resource_names:
-            print(x + ": " + str(self.resources[x]))
+        for x in range(0, 7):
+            if x == 4:
+                print(self.resource_names[x] + ": " + self.stress_status())
+            else:
+                print(self.resource_names[x] + ": " + str(self.resources[x]))
 
     def stress_status(self):
         if self.resources[4] <= 20:
@@ -24,18 +24,16 @@ class Player:
         else:
             return "Nervous"
 
-
     def add(self, other):
-        if other.len() == 7:
-            for x in other:
-                self.resources[x] += x
-
-
+        if len(other) == 7:
+            for x in range(0, 7):
+                self.resources[x] += other[x]
 
 
 p1 = Player()
 p1.get_count()
-p1.add([100, 100, 100, 100, -80, 20, 20])
+myList = [100, 100, 100, 100, -80, 20, 20]
+p1.add(myList)
 p1.get_count()
 
             
